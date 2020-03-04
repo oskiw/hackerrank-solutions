6820d34cef6ab21391d39471b90c92c5ce68d089
oskiw <oskar.wantola@gmail.com>
2020-03-04 20:05:55 +0000

    Sock Merchant solution

---
+++ java/SockMerchant.java
@@ -0,0 +1,56 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class SockMerchant {
+
+    static int sockMerchant(int n, int[] ar) {
+        Arrays.sort(ar);
+
+        if (n < 2) return 0;
+        boolean prevUsed = false;
+        int result = 0;
+
+        for (int i = 1; i < n; i++) {
+            if (ar[i] == ar[i - 1] && !prevUsed) {
+                result++;
+                prevUsed = true;
+            } else {
+                prevUsed = false;
+            }
+        }
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        int[] ar = new int[n];
+
+        String[] arItems = scanner.nextLine().split(" ");
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int i = 0; i < n; i++) {
+            int arItem = Integer.parseInt(arItems[i]);
+            ar[i] = arItem;
+        }
+
+        int result = sockMerchant(n, ar);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
