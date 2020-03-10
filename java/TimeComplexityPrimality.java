5f769ca0945cffbb09dd476864008c476f02803b
oskiw <oskar.wantola@gmail.com>
2020-03-10 01:10:19 +0000

    Time Complexity: Primality solution

---
+++ java/TimeComplexityPrimality.java
@@ -0,0 +1,44 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static String primality(int n) {
+        if (n < 2) return "Not prime";
+        if (n < 4) return "Prime";
+        int max = (int) Math.sqrt(n) + 1;
+        for (int d = 2; d <= max; d++) {
+            if (n % d == 0) return "Not prime";
+        }
+        return "Prime";
+    }
+
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int p = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        for (int pItr = 0; pItr < p; pItr++) {
+            int n = scanner.nextInt();
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            String result = primality(n);
+
+            bufferedWriter.write(result);
+            bufferedWriter.newLine();
+        }
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
