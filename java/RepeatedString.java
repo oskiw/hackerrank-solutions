7839d1629c9db565c6ed44ff5e947a4fa97b6e6b
oskiw <oskar.wantola@gmail.com>
2020-03-14 22:56:04 +0000

    Repeated String solution

---
+++ java/RepeatedString.java
@@ -0,0 +1,47 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class RepeatedString {
+
+    // Complete the repeatedString function below.
+    static long repeatedString(String s, long n) {
+        long as = 0;
+        for (char c : s.toCharArray()) {
+            if (c == 'a') as++;
+        }
+
+        long result = (n / s.length() * as);
+
+        long rest = n % s.length();
+        for (int i = 0; i < rest; i++) {
+            if (s.charAt(i) == 'a') result++;
+        }
+
+        return result;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = scanner.nextLine();
+
+        long n = scanner.nextLong();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+        long result = repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
