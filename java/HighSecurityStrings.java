add4a8f0b2e8646adbc9d08bd656c0ae353b3143
oskiw <oskar.wantola@gmail.com>
2020-03-15 12:35:41 +0000

    HighSecurityStrings solution

---
+++ java/HighSecurityStrings.java
@@ -0,0 +1,48 @@
+package com.example.happypumpkin;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+class Result {
+
+    /*
+     * Complete the 'getStrength' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING password
+     *  2. INTEGER weight_a
+     */
+
+    public static int getStrength(String password, int weight_a) {
+        // Complete the function
+
+        int result = 0;
+        for (char letter : password.toCharArray()) {
+            result += ((letter - 'a') + weight_a) % 26;
+        }
+        return result;
+    }
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        String password = bufferedReader.readLine();
+
+        int weight_a = Integer.parseInt(bufferedReader.readLine().trim());
+
+        int strength = Result.getStrength(password, weight_a);
+
+        bufferedWriter.write(String.valueOf(strength));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
