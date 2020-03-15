439f9f69a901e4edd41090163241bd2125ea1f6c
oskiw <oskar.wantola@gmail.com>
2020-03-15 23:53:22 +0000

    LuckBalance solution

---
+++ java/LuckBalance.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
+
+public class LuckBalance {
+
+    static int luckBalance(int k, int[][] contests) {
+        List<Integer> important = new ArrayList<>();
+        int luck = 0;
+
+        for (int[] contest : contests) {
+            luck += contest[0];
+            if (contest[1] == 1) important.add(contest[0]);
+        }
+
+        Collections.sort(important);
+        for (int i = 0; i < important.size() - k; i++) {
+            luck -= 2 * important.get(i);
+        }
+
+        return luck;
+    }
+
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) throws IOException {
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String[] nk = scanner.nextLine().split(" ");
+
+        int n = Integer.parseInt(nk[0]);
+
+        int k = Integer.parseInt(nk[1]);
+
+        int[][] contests = new int[n][2];
+
+        for (int i = 0; i < n; i++) {
+            String[] contestsRowItems = scanner.nextLine().split(" ");
+            scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+
+            for (int j = 0; j < 2; j++) {
+                int contestsItem = Integer.parseInt(contestsRowItems[j]);
+                contests[i][j] = contestsItem;
+            }
+        }
+
+        int result = luckBalance(k, contests);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedWriter.close();
+
+        scanner.close();
+    }
+}
